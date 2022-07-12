f6a4dedcb820df024252eb6b964656fa42cdb411
Alyssandro Dyogo <100590540+Alyssandro1771415@users.noreply.github.com>
2022-07-12 13:02:07 -0300

    Update cards.component.ts

--- angular_learning/src/app/cards/cards.component.ts
+++ angular_learning/src/app/cards/cards.component.ts
@@ -15,7 +15,7 @@ export class CardsComponent implements OnInit {
 
   cdsInfo:cds[] = [
     {title: 'Cazuza', subtitle: 'Burguesia', img: '/assets/imgs/78bc9d65001134780fa7a752c4f500f2.webp', description:'CD Burguesia do artista Cazuza.'},
-    {title: 'Iron Maiden', subtitle: 'Brave enw world', img: '/assets/imgs/D_NQ_NP_802038-MLB44425430535_122020-O.jpg', description:'CD Brave new world da banda Iron Maid3en'},
+    {title: 'Iron Maiden', subtitle: 'Brave new world', img: '/assets/imgs/D_NQ_NP_802038-MLB44425430535_122020-O.jpg', description:'CD Brave new world da banda Iron Maiden'},
     {title: 'Paralamas do Sucesso', subtitle: 'Arquivo', img: '/assets/imgs/os_paralamas_do_sucesso_arquivo_slide_pack_cd_333468325_1_20201214005157.webp', description:'CD da banda Paralamas do Sucesso.'}
   ]
 
